5f4dafc0ef23629b15b1fd3df0f6342a8fe51467
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2022-06-30 20:44:23 +0430

    take the robot's name from player

---
+++ assets/js/game.js
@@ -0,0 +1,7 @@
+function fight() {
+  window.alert('The fight has begun!');
+}
+// fight();
+
+const playerName = window.prompt("What is your robot's name?");
+console.log(`Our robot's name is ${playerName}`);
